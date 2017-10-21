d419ad25176499e6e49a8daffc3f5a74c238a36c
yinglao <yl3596@columbia.edu>
2017-10-21 18:04:48 -0400

    Add files via upload

---
+++ PokerHand.py
@@ -0,0 +1,95 @@
+"""This module contains a code example related to
+
+Think Python, 2nd Edition
+by Allen Downey
+http://thinkpython2.com
+
+Copyright 2015 Allen Downey
+
+License: http://creativecommons.org/licenses/by/4.0/
+"""
+
+from __future__ import print_function, division
+
+from Card import Hand, Deck
+
+
+class PokerHand(Hand):
+    """Represents a poker hand."""
+
+    def suit_hist(self):
+        """Builds a histogram of the suits that appear in the hand.
+
+        Stores the result in attribute suits.
+        """
+        self.suits = {}
+        for card in self.cards:
+            self.suits[card.suit] = self.suits.get(card.suit, 0) + 1
+
+    def has_flush(self):
+        """Returns True if the hand has a flush, False otherwise.
+      
+        Note that this works correctly for hands with more than 5 cards.
+        """
+        self.suit_hist()
+        for val in self.suits.values():
+            if val >= 5:
+                return True
+        return False
+    def rank_hist(self):
+        """Builds a histogram of the ranks that appear in the hand.
+
+        Stores the result in attribute ranks.
+        """
+        self.ranks = {}
+        for card in self.cards:
+            self.ranks[card.rank] = self.ranks.get(card.rank, 0) + 1
+
+    def has_pair(self):
+        """Returns True if the hand has a pair, False otherwise.
+      
+        Note that this works correctly for hands with more than 2 cards.
+        """
+        self.rank_hist()
+        for val in self.ranks.values():
+            if val >= 2:
+                return True
+        return False
+
+    def has_twopair(self):
+        """Returns True if the hand has a pair, False otherwise.
+      
+        Note that this works correctly for hands with more than 2 cards.
+        """
+        self.rank_hist()
+        n = 0
+        for val in self.ranks.values():
+            if val >= 2:
+                n += 1
+        return n >= 2
+
+    def has_three_of_a_kind(self):
+        self.rank_hist()
+        for val in self.ranks.values():
+            if val >= 3:
+                return True
+        return False
+
+    def has_straight(self):
+        
+
+
+if __name__ == '__main__':
+    # make a deck
+    deck = Deck()
+    deck.shuffle()
+
+    # deal the cards and classify the hands
+    for i in range(7):
+        hand = PokerHand()
+        deck.move_cards(hand, 7)
+        hand.sort()
+        print(hand)
+        print(hand.has_flush())
+        print('')
+
